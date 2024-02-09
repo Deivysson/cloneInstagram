8e112ac98ab8e531fb8f1bcb78103023b4637b61
Deivysson Gomes <105027772+Deivysson@users.noreply.github.com>
2024-02-08 21:49:14 -0300

    Segundo commit
    Criando o header e o feed do clone.

---
+++ src/Lista/index.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  StyleSheet
+} from 'react-native';
+
+class Lista extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            feed: this.props.data
+        };
+    }
+
+
+    render(){
+        return(
+            <View style={styles.areaFeed}>
+
+                <View style={styles.viewPerfil}>
+                    <Image
+                    source={{uri: this.state.feed.imgperfil}}
+                    style={styles.fotoPerfil}
+                    />
+
+                    <Text style={styles.nomeUsuario}> {this.state.feed.nome} </Text>
+                </View>
+
+                <Image 
+                resizeMode='cover'
+                style={styles.fotoPublicacao}
+                source={{uri: this.state.feed.imgPublicacao}}
+                />
+
+                <View style={styles.areaBtn}>
+                    <TouchableOpacity>
+                        <Image 
+                        source={require('../img/like.png')}
+                        style={styles.iconelike}
+                        />
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.btnSend} >
+                        <Image 
+                        source={require('../img/send.png')}
+                        style={styles.iconelike}
+                        />
+                    </TouchableOpacity>
+                </View>
+
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+
+    areaFeed:{
+
+    },
+    nomeUsuario:{
+        fontSize: 22,
+        textAlign: 'left',
+        color: '#000000'
+    },
+    fotoPerfil:{
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+    },
+    fotoPublicacao:{
+        flex: 1,
+        height: 400,
+        alignItems: 'center'
+    },
+    viewPerfil:{
+        flexDirection: 'row',
+        flex: 1,
+        alignItems: 'center',
+        padding: 8,
+    },
+    areaBtn:{
+        flexDirection: 'row',
+        padding: 5
+    },
+    iconelike:{
+        width: 33,
+        height: 33,
+    },
+    btnSend:{
+        paddingLeft: 5,
+    },
+
+});
+
+export default Lista;
